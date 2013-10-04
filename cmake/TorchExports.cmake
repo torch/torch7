04717b44d7c8bd4e7e3e370302f5c6799b30738c
Ronan Collobert <ronan@collobert.com>
2013-10-04 17:27:16 +0200

    torch: fix TorchWrap cmake export

--- cmake/TorchExports.cmake
+++ cmake/TorchExports.cmake
@@ -3,11 +3,12 @@ INSTALL(EXPORT torch-exports
   FILE "TorchExports.cmake")
 
 CONFIGURE_FILE("cmake/TorchConfig.cmake.in" "${CMAKE_CURRENT_BINARY_DIR}/cmake-exports/TorchConfig.cmake" @ONLY)
+CONFIGURE_FILE("cmake/TorchWrap.cmake.in" "${CMAKE_CURRENT_BINARY_DIR}/cmake-exports/TorchWrap.cmake" @ONLY)
 
 INSTALL(
   FILES
   "${CMAKE_CURRENT_BINARY_DIR}/cmake-exports/TorchConfig.cmake"
+  "${CMAKE_CURRENT_BINARY_DIR}/cmake-exports/TorchWrap.cmake"
   "cmake/TorchPathsInit.cmake"
   "cmake/TorchPackage.cmake"
-  "cmake/TorchWrap.cmake"
   DESTINATION "${Torch_INSTALL_CMAKE_SUBDIR}")
