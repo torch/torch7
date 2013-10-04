3501be00a65a4e431cc74d9290d3fa7ee80426ae
Ronan Collobert <ronan@collobert.com>
2013-10-04 17:15:41 +0200

    torch: fix exports

--- cmake/TorchExports.cmake
+++ cmake/TorchExports.cmake
@@ -9,4 +9,5 @@ INSTALL(
   "${CMAKE_CURRENT_BINARY_DIR}/cmake-exports/TorchConfig.cmake"
   "cmake/TorchPathsInit.cmake"
   "cmake/TorchPackage.cmake"
+  "cmake/TorchWrap.cmake"
   DESTINATION "${Torch_INSTALL_CMAKE_SUBDIR}")
