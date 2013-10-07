8e213db486116d5a905cea8d9e02f0c57244b8b9
Ronan Collobert <ronan@collobert.com>
2013-10-07 16:54:05 +0200

    torch: cleanup cmake/rock build

--- cmake/TorchPackage.cmake
+++ cmake/TorchPackage.cmake
@@ -7,13 +7,18 @@ MACRO(ADD_TORCH_PACKAGE package src luasrc)
   ### C/C++ sources
   IF(src)      
 
-    ADD_LIBRARY(${package} SHARED ${src})
+    ADD_LIBRARY(${package} MODULE ${src})
     
     ### Torch packages supposes libraries prefix is "lib"
     SET_TARGET_PROPERTIES(${package} PROPERTIES
       PREFIX "lib"
       IMPORT_PREFIX "lib"
       INSTALL_NAME_DIR "@executable_path/${Torch_INSTALL_BIN2CPATH}")
+    
+    IF(APPLE)
+      SET_TARGET_PROPERTIES(${package} PROPERTIES
+        LINK_FLAGS "-undefined dynamic_lookup")
+    ENDIF()
     
     INSTALL(TARGETS ${package} 
       RUNTIME DESTINATION ${Torch_INSTALL_LUA_CPATH_SUBDIR}
