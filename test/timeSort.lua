5466199d29228bea7dbafa1262a64c1cbbb2863a
Julien Cornebise <julien@cornebise.com>
2013-11-10 20:28:03 +0000

    Update benchmarking script for comparison new/old
    Signed-off-by: Julien Cornebise <jucor@google.com>

--- test/timeSort.lua
+++ test/timeSort.lua
@@ -1,58 +1,84 @@
+-- gnuplot.figure(1)
 -- Test torch sort, show it suffers from the problems of quicksort
 -- i.e. complexity O(N^2) in worst-case of sorted list
 require 'gnuplot'
 
-function testSort(output, descending)
-    descending = descending or false
+function main()
     local pow10 = torch.linspace(1,5,10)
-    local bench_rnd = torch.zeros(pow10:numel())
-    local bench_srt = torch.zeros(pow10:numel())
-    local bench_cst = torch.zeros(pow10:numel())
+    local old_bench_rnd = torch.zeros(pow10:numel())
+    local old_bench_srt = torch.zeros(pow10:numel())
+    local old_bench_cst = torch.zeros(pow10:numel())
+    local new_bench_rnd = torch.zeros(pow10:numel())
+    local new_bench_srt = torch.zeros(pow10:numel())
+    local new_bench_cst = torch.zeros(pow10:numel())
+    local ratio_rnd = torch.zeros(pow10:numel())
+    local ratio_srt = torch.zeros(pow10:numel())
+    local ratio_cst = torch.zeros(pow10:numel())
     local nrep = 3
 
+    -- Ascending sort uses new sort
     local function time_sort(x)
+        collectgarbage()
         local start = os.clock()
-        torch.sort(x,descending)
+        torch.sort(x,false)
+        return (os.clock()-start)
+    end
+
+    -- Descending sort uses old sort
+    local function time_old_sort(x)
+        collectgarbage()
+        local start = os.clock()
+        torch.sort(x,true)
         return (os.clock()-start)
     end
 
     for j = 1,nrep do
         for i = 1,pow10:numel() do
 
-            local this_time
+            local new_time, old_time
             local n = 10^pow10[i]
 
             -- on random
-            this_time = time_sort(torch.rand(n))
-            print('RND j:', j, 'for 10^', pow10[i], ' time: ', this_time)
-            bench_rnd[i] = bench_rnd[i] + this_time/nrep
-            collectgarbage()
+            new_time = time_sort(torch.rand(n))
+            old_time = time_old_sort(torch.rand(n))
+            new_bench_rnd[i] = new_bench_rnd[i] + new_time/nrep
+            old_bench_rnd[i] = old_bench_rnd[i] + old_time/nrep
+            ratio_rnd[i] = ratio_rnd[i] + (old_bench_rnd[i]/new_bench_rnd[i])/nrep
 
             -- on sorted
-            this_time = time_sort(torch.linspace(0,1,n))
-            print('SRT j:', j, 'for 10^', pow10[i], ' time: ', this_time)
-            bench_srt[i] = bench_srt[i] + this_time/nrep
-            collectgarbage()
+            new_time = time_sort(torch.linspace(0,1,n))
+            old_time = time_old_sort(torch.linspace(0,1,n):add(-1):mul(-1)) -- old_time is called on descending sort, hence the reversed input
+            new_bench_srt[i] = new_bench_srt[i] + new_time/nrep
+            old_bench_srt[i] = old_bench_srt[i] + old_time/nrep
+            ratio_srt[i] = ratio_srt[i] + (old_bench_srt[i]/new_bench_srt[i])/nrep
 
             -- on constant
-            this_time = time_sort(torch.zeros(n))
-            print('CST j:', j, 'for 10^', pow10[i], ' time: ', this_time)
-            bench_cst[i] = bench_cst[i] + this_time/nrep
-            collectgarbage()
-
+            new_time = time_sort(torch.zeros(n))
+            old_time = time_old_sort(torch.zeros(n))
+            new_bench_cst[i] = new_bench_cst[i] + new_time/nrep
+            old_bench_cst[i] = old_bench_cst[i] + old_time/nrep
+            ratio_cst[i] = ratio_cst[i] + (old_bench_cst[i]/new_bench_cst[i])/nrep
         end
         io.flush()
     end
-    gnuplot.plot({'Random', pow10, bench_rnd},
-                 {'Sorted', pow10, bench_srt},
-                 {'Constant', pow10, bench_cst})
+    gnuplot.figure(1)
+    gnuplot.plot({'Random - new', pow10, new_bench_rnd},
+                 {'Sorted - new', pow10, new_bench_srt},
+                 {'Constant - new', pow10, new_bench_cst},
+                 {'Random - old', pow10, old_bench_rnd},
+                 {'Sorted - old', pow10, old_bench_srt},
+                 {'Constant - old', pow10, old_bench_cst})
     gnuplot.xlabel('Log10(N)')
     gnuplot.ylabel('Time (s)')
-    gnuplot.figprint(output)
-    print('RND:', bench_rnd)
-    print('SRT:', bench_srt)
-    print('CST:', bench_cst)
+    gnuplot.figprint('benchmarkTime.png')
+
+    gnuplot.figure(2)
+    gnuplot.plot({'Random', pow10, ratio_rnd},
+                 {'Sorted', pow10, ratio_srt},
+                 {'Constant', pow10, ratio_cst})
+    gnuplot.xlabel('Log10(N)')
+    gnuplot.ylabel('Speed-up Factor (s)')
+    gnuplot.figprint('benchmarkRatio.png')
 end
 
-testSort('timeSortAscending.png', false) -- Ascending
-testSort('timeSortDescending.png', true) -- Descending
+main()
