c85e709d619f454356c1dee08bc493f7f352c78b
Andreas Köpf <andreas.koepf@xamla.com>
2016-03-27 13:20:07 +0200

    Add Tensor.chunk() impl not using Tensor.split()

--- test/test.lua
+++ test/test.lua
@@ -3102,7 +3102,7 @@ function torchtest.chunk()
    local result = {}
    local tensor = torch.rand(4,7)
    local nChunk = 3
-   local targetSize = {{4,3},{4,3},{4,1}}
+   local targetSize = {{4,3},{4,2},{4,2}}
    local dim = 2
    local splits = tensor:chunk(nChunk, dim)
    local start = 1
@@ -3111,12 +3111,10 @@ function torchtest.chunk()
       mytester:assertTensorEq(tensor:narrow(dim, start, targetSize[i][dim]), split, 0.00001, 'Content error in chunk '..i)
       start = start + targetSize[i][dim]
    end
-   torch.split(result, tensor, nChunk, dim)
    local start = 1
-   for i, split in ipairs(result) do
-      mytester:assertTableEq(split:size():totable(), targetSize[i], 'Result size error in chunk '..i)
-      mytester:assertTensorEq(tensor:narrow(dim, start, targetSize[i][dim]), split, 0.000001, 'Result content error in chunk '..i)
-      start = start + targetSize[i][dim]
+   for i, split in ipairs(splits) do
+      mytester:assertTensorEq(tensor:narrow(dim, start, split:size(dim)), split, 0.000001, 'Result content error in chunk '..i)
+      start = start + split:size(dim)
    end
 end
 
