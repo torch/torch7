4f99ff44c5f172927baa1e0135acfeb193ace33e
rguthrie3 <rguthrie6595@gmail.com>
2016-08-03 22:26:41 -0500

    SIMD helper header, modified add in THTensorMath to check dispatch

--- lib/TH/THTensor.h
+++ lib/TH/THTensor.h
@@ -36,10 +36,8 @@ typedef struct {
 #include "generic/THTensorMath.h"
 #include "THGenerateAllTypes.h"
 
-
 #include "generic/THTensorMathDispatch.h"
 #include "THGenerateAllTypes.h"
-
 
 /* convolutions */
 #include "generic/THTensorConv.h"
--- lib/TH/generic/THTensorMath.c
+++ lib/TH/generic/THTensorMath.c
@@ -462,7 +462,7 @@ accreal THTensor_(prodall)(THTensor *tensor)
   return prod;
 }
 
-void THTensor_(add_DEFAULT)(THTensor *r_, THTensor *t, real value)
+void THTensor_(add)(THTensor *r_, THTensor *t, real value)
 {
   THTensor_(resizeAs)(r_, t);
   if (THTensor_(isContiguous)(r_) && THTensor_(isContiguous)(t) && THTensor_(nElement)(r_) == THTensor_(nElement)(t)) {
@@ -477,33 +477,6 @@ void THTensor_(add_DEFAULT)(THTensor *r_, THTensor *t, real value)
       TH_TENSOR_APPLY2(real, r_, real, t, *r__data = *t_data + value;);
   }
 }
-
-#if defined(TH_REAL_IS_DOUBLE) 
-
-void THTensor_(add_AVX)(THTensor *r_, THTensor *t, real value)
-{
-  THTensor_(resizeAs)(r_, t);
-  if (THTensor_(isContiguous)(r_) && THTensor_(isContiguous)(t) && THTensor_(nElement)(r_) == THTensor_(nElement)(t)) {
-      real *rp = THTensor_(data)(r_);
-      real *tp = THTensor_(data)(t);
-      ptrdiff_t sz = THTensor_(nElement)(t);
-      ptrdiff_t i = 0;
-      __m256d YMM3 = _mm256_set_pd(value, value, value, value);
-      __m256d YMM0, YMM2;
-      for (; i<=((sz)-4); i+=4) {
-        YMM0 = _mm256_loadu_pd(tp+i);
-        YMM2 = _mm256_add_pd(YMM0, YMM3);
-        _mm256_storeu_pd(rp+i, YMM2);
-      }
-      for (; i<sz; i++) {
-	rp[i] = tp[i] + value;
-      }
-  } else {
-      TH_TENSOR_APPLY2(real, r_, real, t, *r__data = *t_data + value;);
-  }
-}
-
-#endif
 
 void THTensor_(sub)(THTensor *r_, THTensor *t, real value)
 {
