f277ca8aacda22d0c3268195015fb21b510d67e5
Rui Guo <guorui.xt@gmail.com>
2016-09-26 10:45:10 +0800

    bug fix for read/writeLong in THMemoryFile

--- test/test_sharedmem.lua
+++ test/test_sharedmem.lua
@@ -1,4 +1,5 @@
 require 'torch'
+local ffi = require 'ffi'
 
 local tester = torch.Tester()
 local tests = torch.TestSuite()
@@ -13,38 +14,52 @@ local function createSharedMemStorage(name, size, storageType)
   return storage, shmName
 end
 
+local function shmFilePath(shmName)
+  return (ffi.os ~= 'Windows' and '/dev/shm/' or '') .. shmName
+end
+
+local function removeShmFile(shmFileName)
+  if ffi.os == 'Windows' then
+    os.remove(shmFileName)
+  end
+end
+
 function tests.createSharedMemFile()
   local storage, shmName = createSharedMemStorage()
+  local shmFileName = shmFilePath(shmName)
 
   -- check that file is at /dev/shm
-  tester:assert(paths.filep('/dev/shm/' .. shmName),
-                'Shared memory file does not exist')
+  tester:assert(paths.filep(shmFileName),
+                'Shared memory file exists')
 
   -- collect storage and make sure that file is gone
   storage = nil
   collectgarbage()
   collectgarbage()
-  tester:assert(not paths.filep('/dev/shm/' .. shmName),
-                'Shared memory file still exists')
+  removeShmFile(shmFileName)
+  tester:assert(not paths.filep(shmFileName),
+                'Shared memory file does not exists')
 end
 
 function tests.checkContents()
   local storage, shmName = createSharedMemStorage()
+  local shmFileName = shmFilePath(shmName)
   local tensor = torch.FloatTensor(storage, 1, torch.LongStorage{storage:size()})
   tensor:copy(torch.rand(storage:size()))
 
-  local sharedFile = torch.DiskFile('/dev/shm/'..shmName, 'r'):binary()
+  local sharedFile = torch.DiskFile(shmFileName, 'r'):binary()
   for i = 1, storage:size() do
     tester:assert(sharedFile:readFloat() == storage[i], 'value is not correct')
   end
   sharedFile:close()
+  removeShmFile(shmFileName)
 end
 
 function tests.testSharing()
   -- since we are going to cast numbers into double (lua default)
   -- we specifically generate double storage
   local storage, shmName = createSharedMemStorage(nil, nil, 'DoubleStorage')
-  local shmFileName = '/dev/shm/' .. shmName
+  local shmFileName = shmFilePath(shmName)
   local tensor = torch.DoubleTensor(storage, 1, torch.LongStorage{storage:size()})
   tensor:copy(torch.rand(storage:size()))
   local tensorCopy = tensor.new():resizeAs(tensor):copy(tensor)
@@ -70,6 +85,7 @@ function tests.testSharing()
   for i = 1, tensor:size(1) do
     tester:asserteq(tensor[i], rval, 'content is wrong')
   end
+  removeShmFile(shmFileName)
 end
 
 tester:add(tests)
