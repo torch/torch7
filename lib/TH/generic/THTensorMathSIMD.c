7873b99544fb5e42665d2bc751d2e6127442e8e3
jokeren <robinho364@gmail.com>
2016-12-03 16:46:47 +0800

    Fix multi-thread bugs

--- lib/TH/generic/THTensorMathSIMD.c
+++ lib/TH/generic/THTensorMathSIMD.c
@@ -10,9 +10,6 @@
 
 #include <omp.h>
 
-#define TH_OMP_OVERHEAD_THRESHOLD_TYPE(TYPE) ((256/sizeof(TYPE))*100000)
-#define MIN(a, b) (a) > (b) ? (a) : (b);
-
 #if defined(TH_REAL_IS_DOUBLE)
 
 void THTensor_(add_AVX)(THTensor *r_, THTensor *t, real value)
@@ -22,26 +19,21 @@ void THTensor_(add_AVX)(THTensor *r_, THTensor *t, real value)
     real *rp = THTensor_(data)(r_);
     real *tp = THTensor_(data)(t);
     ptrdiff_t sz = THTensor_(nElement)(t);
-    ptrdiff_t i = 0;
-    #pragma omp parallel if(sz > TH_OMP_OVERHEAD_THRESHOLD_TYPE(double)) private(i)
+    #pragma omp parallel if(sz > TH_OMP_OVERHEAD_THRESHOLD)
     {
-      ptrdiff_t i_start = i * sz / omp_get_num_threads();
-      ptrdiff_t i_end = MIN(i_start + sz / omp_get_num_threads(), sz)
+      size_t num_threads = omp_get_num_threads();
+      size_t tid = omp_get_thread_num();
+      ptrdiff_t i = tid * (sz / num_threads);
+      ptrdiff_t i_end = tid == num_threads - 1 ? sz : i + sz / num_threads;
       __m256d YMM15 = _mm256_set_pd(value, value, value, value);
-      __m256d YMM0, YMM1, YMM2, YMM3, YMM4, YMM5, YMM6, YMM7;
-      for (; i<=((i_end)-16); i+=16) {
+      __m256d YMM0, YMM1, YMM2, YMM3, YMM4, YMM5;
+      for (; i<=((i_end)-8); i+=8) {
         YMM0 = _mm256_loadu_pd(tp+i);
         YMM1 = _mm256_loadu_pd(tp+i+4);
-        YMM2 = _mm256_loadu_pd(tp+i+8);
-        YMM3 = _mm256_loadu_pd(tp+i+12);
         YMM4 = _mm256_add_pd(YMM0, YMM15);
         YMM5 = _mm256_add_pd(YMM1, YMM15);
-        YMM6 = _mm256_add_pd(YMM2, YMM15);
-        YMM7 = _mm256_add_pd(YMM3, YMM15);
         _mm256_storeu_pd(rp+i, YMM4);
         _mm256_storeu_pd(rp+i+4, YMM5);
-        _mm256_storeu_pd(rp+i+8, YMM6);
-        _mm256_storeu_pd(rp+i+12, YMM7);
       }
       for (; i<i_end; i++) {
         rp[i] = tp[i] + value;
@@ -59,16 +51,25 @@ void THTensor_(mul_AVX)(THTensor *r_, THTensor *t, real value)
     real *tp = THTensor_(data)(t);
     real *rp = THTensor_(data)(r_);
     ptrdiff_t sz = THTensor_(nElement)(t);
-    ptrdiff_t i = 0;
-    __m256d YMM3 = _mm256_set_pd(value, value, value, value);
-    __m256d YMM0, YMM2;
-    for (; i<=((sz)-4); i+=4) {
-      YMM0 = _mm256_loadu_pd(tp+i);
-      YMM2 = _mm256_mul_pd(YMM0, YMM3);
-      _mm256_storeu_pd(rp+i, YMM2);
-    }
-    for (; i<sz; i++) {
-      rp[i] = tp[i] * value;
+    #pragma omp parallel if(sz > TH_OMP_OVERHEAD_THRESHOLD)
+    {
+      size_t num_threads = omp_get_num_threads();
+      size_t tid = omp_get_thread_num();
+      ptrdiff_t i = tid * (sz / num_threads);
+      ptrdiff_t i_end = tid == num_threads - 1 ? sz : i + sz / num_threads;
+      __m256d YMM15 = _mm256_set_pd(value, value, value, value);
+      __m256d YMM0, YMM1, YMM2, YMM3, YMM4, YMM5;
+      for (; i<=((i_end)-8); i+=8) {
+        YMM0 = _mm256_loadu_pd(tp+i);
+        YMM1 = _mm256_loadu_pd(tp+i+4);
+        YMM4 = _mm256_mul_pd(YMM0, YMM15);
+        YMM5 = _mm256_mul_pd(YMM1, YMM15);
+        _mm256_storeu_pd(rp+i, YMM4);
+        _mm256_storeu_pd(rp+i+4, YMM5);
+      }
+      for (; i<i_end; i++) {
+        rp[i] = tp[i] * value;
+      }
     }
   } else {
     TH_TENSOR_APPLY2(real, r_, real, t, *r__data = *t_data * value;);
@@ -82,16 +83,25 @@ void THTensor_(div_AVX)(THTensor *r_, THTensor *t, real value)
     real *tp = THTensor_(data)(t);
     real *rp = THTensor_(data)(r_);
     ptrdiff_t sz = THTensor_(nElement)(t);
-    ptrdiff_t i = 0;
-    __m256d YMM3 = _mm256_set_pd(value, value, value, value);
-    __m256d YMM0, YMM2;
-    for (; i<=((sz)-4); i+=4) {
-      YMM0 = _mm256_loadu_pd(tp+i);
-      YMM2 = _mm256_div_pd(YMM0, YMM3);
-      _mm256_storeu_pd(rp+i, YMM2);
-    }
-    for (; i<sz; i++) {
-      rp[i] = tp[i] / value;
+    #pragma omp parallel if(sz > TH_OMP_OVERHEAD_THRESHOLD)
+    {
+      size_t num_threads = omp_get_num_threads();
+      size_t tid = omp_get_thread_num();
+      ptrdiff_t i = tid * (sz / num_threads);
+      ptrdiff_t i_end = tid == num_threads - 1 ? sz : i + sz / num_threads;
+      __m256d YMM15 = _mm256_set_pd(value, value, value, value);
+      __m256d YMM0, YMM1, YMM2, YMM3, YMM4, YMM5, YMM6, YMM7;
+      for (; i<=((i_end)-8); i+=8) {
+        YMM0 = _mm256_loadu_pd(tp+i);
+        YMM1 = _mm256_loadu_pd(tp+i+4);
+        YMM4 = _mm256_div_pd(YMM0, YMM15);
+        YMM5 = _mm256_div_pd(YMM1, YMM15);
+        _mm256_storeu_pd(rp+i, YMM4);
+        _mm256_storeu_pd(rp+i+4, YMM5);
+      }
+      for (; i<i_end; i++) {
+        rp[i] = tp[i] / value;
+      }
     }
   } else {
     TH_TENSOR_APPLY2(real, r_, real, t, *r__data = *t_data / value;);
@@ -109,26 +119,21 @@ void THTensor_(add_AVX)(THTensor *r_, THTensor *t, real value)
     real *rp = THTensor_(data)(r_);
     real *tp = THTensor_(data)(t);
     ptrdiff_t sz = THTensor_(nElement)(t);
-    ptrdiff_t i = 0;
-    __m256 YMM15 = _mm256_set_ps(value, value, value, value, value, value, value, value);
-    __m256 YMM0, YMM1, YMM2, YMM3, YMM4, YMM5, YMM6, YMM7;
-    #pragma omp parallel if(sz > TH_OMP_OVERHEAD_THRESHOLD_TYPE(float)) private(i)
+    #pragma omp parallel if(sz > TH_OMP_OVERHEAD_THRESHOLD)
     {
-      ptrdiff_t i_start = i * sz / omp_get_num_threads();
-      ptrdiff_t i_end = MIN(i_start + sz / omp_get_num_threads(), sz)
-      for (; i<=((i_end)-32); i+=32) {
+      size_t num_threads = omp_get_num_threads();
+      size_t tid = omp_get_thread_num();
+      ptrdiff_t i = tid * (sz / num_threads);
+      ptrdiff_t i_end = tid == num_threads - 1 ? sz : i + sz / num_threads;
+      __m256 YMM15 = _mm256_set_ps(value, value, value, value, value, value, value, value);
+      __m256 YMM0, YMM1, YMM2, YMM3, YMM4, YMM5, YMM6, YMM7;
+      for (; i<=((i_end)-16); i+=16) {
         YMM0 = _mm256_loadu_ps(tp+i);
         YMM1 = _mm256_loadu_ps(tp+i+8);
-        YMM2 = _mm256_loadu_ps(tp+i+16);
-        YMM3 = _mm256_loadu_ps(tp+i+24);
         YMM4 = _mm256_add_ps(YMM0, YMM15);
         YMM5 = _mm256_add_ps(YMM1, YMM15);
-        YMM6 = _mm256_add_ps(YMM2, YMM15);
-        YMM7 = _mm256_add_ps(YMM3, YMM15);
         _mm256_storeu_ps(rp+i, YMM4);
         _mm256_storeu_ps(rp+i+8, YMM5);
-        _mm256_storeu_ps(rp+i+16, YMM6);
-        _mm256_storeu_ps(rp+i+24, YMM7);
       }
       for (; i<i_end; i++) {
         rp[i] = tp[i] + value;
@@ -146,16 +151,25 @@ void THTensor_(mul_AVX)(THTensor *r_, THTensor *t, real value)
     real *tp = THTensor_(data)(t);
     real *rp = THTensor_(data)(r_);
     ptrdiff_t sz = THTensor_(nElement)(t);
-    ptrdiff_t i = 0;
-    __m256 YMM3 = _mm256_set_ps(value, value, value, value, value, value, value, value);
-    __m256 YMM0, YMM2;
-    for (; i<=((sz)-8); i+=8) {
-      YMM0 = _mm256_loadu_ps(tp+i);
-      YMM2 = _mm256_mul_ps(YMM0, YMM3);
-      _mm256_storeu_ps(rp+i, YMM2);
-    }
-    for (; i<sz; i++) {
-      rp[i] = tp[i] * value;
+    #pragma omp parallel if(sz > TH_OMP_OVERHEAD_THRESHOLD)
+    {
+      size_t num_threads = omp_get_num_threads();
+      size_t tid = omp_get_thread_num();
+      ptrdiff_t i = tid * (sz / num_threads);
+      ptrdiff_t i_end = tid == num_threads - 1 ? sz : i + sz / num_threads;
+      __m256 YMM15 = _mm256_set_ps(value, value, value, value, value, value, value, value);
+      __m256 YMM0, YMM1, YMM2, YMM3, YMM4, YMM5, YMM6, YMM7;
+      for (; i<=((i_end)-16); i+=16) {
+        YMM0 = _mm256_loadu_ps(tp+i);
+        YMM1 = _mm256_loadu_ps(tp+i+8);
+        YMM4 = _mm256_mul_ps(YMM0, YMM15);
+        YMM5 = _mm256_mul_ps(YMM1, YMM15);
+        _mm256_storeu_ps(rp+i, YMM4);
+        _mm256_storeu_ps(rp+i+8, YMM5);
+      }
+      for (; i<i_end; i++) {
+        rp[i] = tp[i] * value;
+      }
     }
   } else {
     TH_TENSOR_APPLY2(real, r_, real, t, *r__data = *t_data * value;);
@@ -169,16 +183,25 @@ void THTensor_(div_AVX)(THTensor *r_, THTensor *t, real value)
     real *tp = THTensor_(data)(t);
     real *rp = THTensor_(data)(r_);
     ptrdiff_t sz = THTensor_(nElement)(t);
-    ptrdiff_t i = 0;
-    __m256 YMM3 = _mm256_set_ps(value, value, value, value, value, value, value, value);
-    __m256 YMM0, YMM2;
-    for (; i<=((sz)-8); i+=8) {
-      YMM0 = _mm256_loadu_ps(tp+i);
-      YMM2 = _mm256_div_ps(YMM0, YMM3);
-      _mm256_storeu_ps(rp+i, YMM2);
-    }
-    for (; i<sz; i++) {
-      rp[i] = tp[i] / value;
+    #pragma omp parallel if(sz > TH_OMP_OVERHEAD_THRESHOLD)
+    {
+      size_t num_threads = omp_get_num_threads();
+      size_t tid = omp_get_thread_num();
+      ptrdiff_t i = tid * (sz / num_threads);
+      ptrdiff_t i_end = tid == num_threads - 1 ? sz : i + sz / num_threads;
+      __m256 YMM15 = _mm256_set_ps(value, value, value, value, value, value, value, value);
+      __m256 YMM0, YMM1, YMM2, YMM3, YMM4, YMM5;
+      for (; i<=((i_end)-16); i+=16) {
+        YMM0 = _mm256_loadu_ps(tp+i);
+        YMM1 = _mm256_loadu_ps(tp+i+8);
+        YMM4 = _mm256_div_ps(YMM0, YMM15);
+        YMM5 = _mm256_div_ps(YMM1, YMM15);
+        _mm256_storeu_ps(rp+i, YMM4);
+        _mm256_storeu_ps(rp+i+8, YMM5);
+      }
+      for (; i<i_end; i++) {
+        rp[i] = tp[i] / value;
+      }
     }
   } else {
     TH_TENSOR_APPLY2(real, r_, real, t, *r__data = *t_data / value;);
