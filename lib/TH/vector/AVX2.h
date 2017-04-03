d3999bc32c84c1d41c129d14482ee5b873c9bbc6
Eric Cosatto <cosatto@nec-labs.com>
2017-04-03 17:35:38 -0400

    revert

--- lib/TH/vector/AVX2.h
+++ lib/TH/vector/AVX2.h
@@ -2,9 +2,8 @@
 #define TH_AVX2_H
 
 #include <stddef.h>
-#include "THGeneral.h"
 
-TH_API void THDoubleVector_cadd_AVX2(double *z, const double *x, const double *y, const double c, const ptrdiff_t n);
-TH_API void THFloatVector_cadd_AVX2(float *z, const float *x, const float *y, const float c, const ptrdiff_t n);
+void THDoubleVector_cadd_AVX2(double *z, const double *x, const double *y, const double c, const ptrdiff_t n);
+void THFloatVector_cadd_AVX2(float *z, const float *x, const float *y, const float c, const ptrdiff_t n);
 
 #endif
